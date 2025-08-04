e68c8d33f7a6c9e5741784d0ebd43ff25913c2a9
Pax <pax-flutter@gate.me>
2025-08-04 19:09:48 +0800

    chore: 添加Gate测试环境

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,8 @@ import './header.scss';
 
 export const Header = () => {
     return <header>
-        <span>My App with React UI</span>
+        <span>欢迎Lumia前来测试</span>
+        <img style={{width: '100px', height: '100px'}} src='/20250804-185834.jpeg' alt="logo" />
         <TonConnectButton />
     </header>
 }
