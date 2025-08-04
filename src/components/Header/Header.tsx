4b77eadb85e59b751689cd987fd45db70fb16735
Pax <pax-flutter@gate.me>
2025-08-04 20:41:07 +0800

    chore: 更新产物

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,6 @@ import './header.scss';
 export const Header = () => {
     return <header>
         <span>欢迎Lumia前来测试</span>
-        <img style={{width: '100px', height: '100px'}} src='/20250804-185834.jpeg' alt="logo" />
         <TonConnectButton />
     </header>
 }
